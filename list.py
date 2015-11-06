e6d589129350cad9dbc3c57d14b1bc388ca35ad5
minwoobae <minubae.nyc@gmail.com>
2015-11-05 20:06:12 -0500

    created recursion.py and modified list.py

--- list.py
+++ list.py
@@ -3,25 +3,34 @@
 # Author: Minwoo Bae (minubae.nyc@gmail.com)
 # Reference: http://wphooper.com/teaching/2015-fall-308/python/Lists.html
 
-## Example 01
-# The tribonacci sequence is a sequence of integers defined inductively by a0=a1=0, a2=1, and an+3=an+an+1+an+2 for integers n≥0. 
-# Write a function tribonacci(m) which takes as input an number m≥1 and returns the list [a0,a1,a2,…,ak] where ak is 
-# the largest number in the sequence with ak<m.
+def fibonacci_list(n):
+    flist = [1,1]
+    while len(flist)<n:
+        flist.append(flist[len(flist)-1]+flist[len(flist)-2])
+    return flist
 
-## Example 02
+    
+## Example 01
 # A list is a palindrome if it is the same when reversed. Write a function is_palindrome(lst) which takes as input a list lst and 
 # returns the truth value of the statement "lst is a palindrome."
-
-## Example 03
+def is_palindrome(lst):
+	return 1
+## Example 02
 # Write a function derivative(c) which takes as input a list c coefficients of a polynomial and returns the coefficient list of 
 # the derivative of the polynomial.
+def derivative(c):
+	return 1
 
-## Example 04
+## Example 03
 # Write a function multiply_polynomials(c1,c2) which takes as input two lists of coefficients of two polynomials and 
 # returns the coefficient list of the product of these polynomials.
+def multiply_polynomials(c1,c2):
+	return 1
 
-## Example 05
+## Example 04
 # A polygon in the plane can be stored as a list of coordinates of vertices, where each vertex is a pair of numbers. 
 # For instance [(0,0),(3,0),(0,4)] represents a 3-4-5 right triangle.
 # Write a function perimeter(p) which computes the perimeter of the polygon p, where p is represented as a list of coordinates as above. 
 # For example, perimeter([(0,0),(3,0),(0,4)]) should return 12 (which is 3+4+5).
+def perimeter(p):
+	return 1
---
+++ recursion.py
@@ -0,0 +1,57 @@
+# Title: Examples of Mathematics Logic and Set in Python
+# Date: Oct/27/2015, Tuesday
+# Author: Minwoo Bae (minubae.nyc@gmail.com)
+# Reference: http://wphooper.com/teaching/2015-fall-308/python/Recursion.html
+
+import math
+
+
+## From Examples:
+def factorial(n):
+    if n==0:
+        return 1
+    return n*factorial(n-1)
+
+## Pascal's triangle:
+# Consider a polynomial p(t)=aktk+ak−1tk−1+…+a1t+a0. Observe that
+# (t+1)p(t)=aktk+1+(ak+ak−1)tk+(ak−1+ak−2)tk−1+…+(a0+a1)t+a0.
+# We can define a sequence of polynomials by q0=1 and inductively defining qn+1(t)=(t+1)qn(t) for integers n≥0. 
+# Then each qn(t) is a polynomial of degree n. We define an,k to be the coefficient of the tk term of the polynomial qn(t). 
+# When arranged into a rectangular array, these numbers are known as Pascal's triangle.
+# Problem: Write a recursive function pascals_triangle(n,k) which returns an,k 
+# when provided an integer n≥0 and an integer k satisfying 0≤k≤n.
+# To do this, first observe that the constant term of qn(t) is always one, as is the coefficient of tn in qn(t). 
+# That is, qn,0=1 and qn,n=1 for all integers n≥0.
+# Second, using the formula relating the coefficients for (t+1)p(t) to the coefficients for p(t) above, 
+# we see that an,k=an−1,k−1+an−1,k when 0<k<n.
+def pascals_triangle(n,k):
+    if k==0 or k==n:
+        return 1
+    return pascals_triangle(n-1,k-1)+pascals_triangle(n-1,k)
+
+def expansion(n,b):
+    if n<b:
+        return [n]
+    a0 = n%b
+    lst=expansion((n-a0)//b, b)
+    lst.insert(0,a0)
+    return lst
+
+## Example 01
+# (Catalan Numbers) The Catalan numbers a sequence of integers defined inductively via C0=1 and Cn+1=2(2n+1)n+2Cn for integers n≥0. 
+# Define a recursive function catalan(n) which takes as input an integer i≥0 and returns the Catalan number Ci. 
+# (The Catalan numbers first appeared as an exercise in the page on Lists.)
+
+## Example 02
+# (Greatest common divisor) Write a recursive function gcd(m,n) which returns the greatest common divisor of 
+# integers m≥0 and n≥0 (not both zero). Use the following observations:
+# gcd(m,n)=gcd(n,m) for all m and n.
+# gcd(0,n)=n when n>0.
+# gcd(m,n)=gcd(n%m,m) whenever 0<m≤n.
+
+
+
+
+
+
+
