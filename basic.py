462bd3dc9ea93a4ad0423bf12acdbacc6daa4db8
minwoobae <minubae.nyc@gmail.com>
2016-01-11 15:50:19 -0500

    update basic.py

--- basic.py
+++ basic.py
@@ -1,46 +1,53 @@
-# Title: Examples of Basic Mathematics in Python
-# Date: Oct/06/2015, Tuesday
-# Author: Minwoo Bae (mbae000@citymail.cuny.edu)
+# Title: Mathematical Algorithms Basics in Python
+# Date: Oct/06/2015, Tuesday - Current
+# Author: Minwoo Bae (minubae.nyc@gmail.com)
 # Reference: http://wphooper.com/teaching/2015-fall-308/python/Numbers.html
 
 import math
-# from sympy import * # Import the Sympy library for the computation of Calculus 
 
-## Example 01: 
-# Write a Python function print_divisor(p) which takes as input an integer p >= 1 
-# and prints all its positive divisors.
-def print_divisor(p):
-	i=1
-	while i < p:
-		if p%i == 0:
-			print('Divisor: ', i)
-		i += 1
-# print('Enter an integer number p >= 1: ')
-# num1 = int(input())
-# print(print_divisor(num1))
+# 01) Find Divisors of a Natural Number P 
+# Write a Python function print_divisor(p) which takes as input an integer p >= 1 and prints all its positive divisors.
 
-## Example 02:
-# Write a Python function called factorial(n) which takes as input a positive integer n 
-# and return n!.
-def factorial(n):
-	i = 1
-	fact = 1
-	if n == 0 or n == 1:
-		return 1
+# Using while_statement
+def print_divisor_01(p):
+        i=1
+        temp=list()
+        while i < p:
+                if p%i == 0:
+                        temp.append(i)
+                i += 1
+        return temp
 
-	# Recursive solution: 
-	# fact = factorial(n-1) * n
-	# return fact
+# Using for_statement
+def print_divisor_02(p):
+        temp=list()
+        for i in range(1,p):
+                if p%i == 0:
+                        temp.append(i)
+        return temp
 
-	while i <= n:
-		fact = fact*i
-		i += 1
+# 02) Find a Factorial of a Non-negative Integer N
+# Write a Python function called factorial(n) which takes as input a positive integer n and return n!.
 
-	return fact
+# Using while_statement
+def factorial_01(n):
+        i = 1
+        fact = 1
+        if n==0 or n==1:
+                return 1
+        while i<=n:
+                fact = fact*i
+                i+=1
+        return fact
 
-# print('Enter a positive integer n: ')
-# num2 = int(input())
-# print(factorial(num2))
+# Using for_statement
+def factorial_02(n):
+        fact = 1
+        if n==0 or n==1:
+                return 1
+        for i in range(1,n+1):
+                fact = fact*i
+        return fact
 
 ## Example 03:
 # Write a Python function called is_square(n) which takes as input an integer n 
